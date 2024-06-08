b5369376f9df82c2b3fb79db1d80dcdc341c813b
Danny Huynh <47933673+DevilLucker@users.noreply.github.com>
2024-06-08 11:15:14 +0700

    Update ad.lua

--- ad.lua
+++ ad.lua
@@ -17,7 +17,15 @@ pcall(function()
             pvData['robloxUser'] = game:GetService('Players').LocalPlayer.Name
             pvData['note'] = note
             pvData['content'] = {}
+            pvData['content']['Fighting Style'] = {}
             pvData['content']['Data'] = {}
+            pvData['content']['Inventory'] = {}
+            pvData['content']['Inventory']['Sword'] = {}
+            pvData['content']['Inventory']['Wear'] = {}
+            pvData['content']['Inventory']['Gun'] = {}
+            pvData['content']['Inventory']['Blox Fruit'] = {}
+            pvData['content']['Inventory']['Material'] = {}
+            pvData['content']['Awakened Abilities'] = {}
 
 
             -- * Gems
@@ -31,7 +39,7 @@ pcall(function()
                 local numValue_gems_value = tonumber(numString_gems_value)
 
                 -- In ra số nguyên
-                -- print(numValue_gems_value)
+                print(numValue_gems_value)
                 pvData['content']['Data']['Fragments'] = numValue_gems_value
 
                 warn('Gems')
@@ -48,7 +56,7 @@ pcall(function()
                 local numValue_gold_value = tonumber(numString_gold_value)
 
                 -- In ra số nguyên
-                --print(numValue_gold_value)
+                print(numValue_gold_value)
                 pvData['content']['Data']['Beli'] = numValue_gold_value
 
                 warn('Gold')
@@ -70,50 +78,43 @@ pcall(function()
                 warn('Level')
             end)
 
+            for i, v in next, pvData do
+                print(i, v)
+            end
+
             -- ! post
             local url = 'https://paulvoid.com/api/ram/update';
             local Request = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 
-            -- jobid
-            local jobid = game.JobId
-            local enemies = game:GetService("Workspace").Enemies:GetChildren()
-            -- get list of enemies
-            local enemyList = {}
-            for i, v in next, enemies do
-
-                table.insert(enemyList, v.Name)
+            local success, response = pcall(function()
+                return Request({
+                    Url = url,
+                    Method = "POST",
+                    Headers = {
+                        ["Content-Type"] = "application/json"
+                    },
+                    Body = game:GetService("HttpService"):JSONEncode(pvData)
+                })
+            end)
+            
+            if success then
+                if response then
+                    if response.StatusCode then
+                        if response.StatusCode == 200 then
+                            print("Data was posted successfully")
+                        else
+                            print("Failed to post data, status code: ", response.StatusCode)
+                        end
+                    else
+                        print("No StatusCode in the response")
+                    end
+                else
+                    print("No response from the server")
+                end
+            else
+                print("Failed to send the request: ", response)
             end
-            -- get placeid
-            local placeid = game.PlaceId
-            -- get players length
-            local players = game:GetService("Players"):GetPlayers()
-            local playersLength = #players
-
-            -- world
-
-            local responses = Request({
-                Url = url,
-                Method = "POST",
-                Headers = {
-                    ["Content-Type"] = "application/json"
-                },
-                Body = game:GetService("HttpService"):JSONEncode(pvData)
-            }
-            )
-
-            --local responses1 = Request({
-            --    Url = "https://paulvoid.com/api/script/serverInfo",
-            --    Method = "POST",
-            --    Headers = {
-            --        ["Content-Type"] = "application/json"
-            --    },
-            --    Body = game:GetService("HttpService"):JSONEncode({
-            --        ["jobID"] = jobid,
-            --        ["placeID"] = placeid,
-            --        ["boss"] = enemyList,
-            --        ["slot"] = playersLength
-            --    })
-            --})
+            
             print(responses.Body)
         end)
         task.wait(delay)
